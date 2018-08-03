6c948365245138841f09f16c1b1d74a49237e465
Alexandru Miron <mironalex96@gmail.com>
2018-08-03 20:51:49 +0300

    keeping secrets in .env now

---
+++ config.js
@@ -0,0 +1,7 @@
+//token for discord bot access
+//put your token in a file called .env
+//for example:
+//TOKEN=YOUR_TOKEN_HERE
+exports.token = process.env.TOKEN;
+exports.adminUser = process.env.ADMIN;
+
